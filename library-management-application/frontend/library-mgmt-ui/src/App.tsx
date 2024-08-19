ec8f0cacd09df49cb53ba2c937862e4dfd199a7a
Sagnik004 <sagnikchakraborty910@yahoo.com>
2024-08-20 01:33:40 +0530

    home page carousel template added

--- library-management-application/frontend/library-mgmt-ui/src/App.tsx
+++ library-management-application/frontend/library-mgmt-ui/src/App.tsx
@@ -3,12 +3,14 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/layouts/navbar/Navbar';
 import ExploreTopBooks from './components/layouts/homePage/ExploreTopBooks';
+import Carousel from './components/layouts/homePage/Carousel';
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <ExploreTopBooks />
+      <Carousel />
     </div>
   );
 };
